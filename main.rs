7215e6b7734a7795d1e43249fd4702621ad4ba2e
Eric Holk <eric.holk@gmail.com>
2012-12-21 14:58:12 -0800

    Added timing code.

--- main.rs
+++ main.rs
@@ -1,9 +1,12 @@
+extern mod std;
+use std::time::precise_time_s;
+
 extern mod OpenCL;
 
 use OpenCL::hl::*;
 use OpenCL::vector::*;
 
-const N: uint = 1200;
+const N: uint = 1024;
 
 fn main() {
     let ctx = create_compute_context();
@@ -37,14 +40,62 @@ fn main() {
     }
 
     let k = program.create_kernel("MyAdd");
-    
-    k.set_arg(0, &A);
-    k.set_arg(1, &B);
-    k.set_arg(2, &C);
+
+    let time = time_kernel(ctx, &k, &A, &B, &C);
+
+    io::println(fmt!("MyAdd:\t%? msec/kernel", time));
+}
+
+fn time_kernel(ctx: @ComputeContext, k: &Kernel,
+               A: &Vector<f64>,
+               B: &Vector<f64>,
+               C: &Vector<f64>) -> float
+{
+    const REP: uint = 500;
+
+    const LOCAL_SIZE: int = 256;
+
+    k.set_arg(0, A);
+    k.set_arg(1, B);
+    k.set_arg(2, C);
     k.set_arg(3, &N);
 
+    // Call the timer once to warm up.
+    precise_time_s();
+
+    // Do it once to avoid timing inconsistencies.
     enqueue_nd_range_kernel(
         &ctx.q,
-        &k,
-        1, 0, N as int, 1);
+        k,
+        1, 0, N as int, LOCAL_SIZE);
+
+    // Loop one
+    let start1 = precise_time_s();
+    for REP.times || {
+        enqueue_nd_range_kernel(
+            &ctx.q,
+            k,
+            1, 0, N as int, LOCAL_SIZE);
+    }
+    let stop1 = precise_time_s();
+
+    let elapsed1 = stop1 - start1;
+
+    // Loop two
+    let start2 = precise_time_s();
+    for REP.times || {
+        enqueue_nd_range_kernel(
+            &ctx.q,
+            k,
+            1, 0, N as int, LOCAL_SIZE);
+        enqueue_nd_range_kernel(
+            &ctx.q,
+            k,
+            1, 0, N as int, LOCAL_SIZE);
+    }
+    let stop2 = precise_time_s();
+
+    let elapsed2 = stop2 - start2;
+
+    (elapsed2 - elapsed1) / (REP as float) * 1000000f
 }
